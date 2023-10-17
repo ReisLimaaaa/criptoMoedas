1a0f5f9747eb4dbf4e0636dad95835c084526524
Rebeca Araujo <104568778+ReisLimaaaa@users.noreply.github.com>
2023-10-17 12:40:41 +0000

    ...

--- criptoMoedas/src/Components/Header/index.tsx
+++ criptoMoedas/src/Components/Header/index.tsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom"
+import styles from "./header.module.css"
+import logoimg from "../../assets/logo.png"
+
 export function Header(){
     return(
-        <header>
-            <h1>Header</h1>
+        <header className={styles.container}>
+            <div className={styles.logo}>
+                <Link to="/">
+                    <img src={logoimg} alt="Logo CriptoMoedas"/>
+                </Link>
+
+            </div>
+            
         </header>
     )
 }
--- criptoMoedas/src/Pages/Home/index.tsx
+++ criptoMoedas/src/Pages/Home/index.tsx
@@ -1,9 +1,53 @@
+import { Link } from "react-router-dom";
 import styles from "./home.module.css"
+import { HiMiniMagnifyingGlass } from "react-icons/hi2";
 
 export function Home (){
     return(
-        <div className={styles.container}>
-            <h1>Pagina Home</h1>
-        </div>
+        <main className={styles.container}>
+            <form className={styles.form}>
+                <input 
+                placeholder="Digite o simbolo da moeda: BTC.."
+                />
+                <button type="submit">
+                <HiMiniMagnifyingGlass size={30} color="#fff"/>
+                </button>
+
+            </form>
+                
+                <table>
+                <thead>
+                    <tr>
+
+                        <th scope="col">Moedas</th>
+                        <th scope="col">Valor Mercado</th>
+                        <th scope="col">Preço</th>
+                        <th scope="col">Volume</th>
+
+                    </tr>
+                </thead>
+
+                    <tbody id="tbody">
+                        <tr className={styles.tr}>
+                            <td className={styles.tdLabel} data-label="Moeda">
+                                <Link className={styles.link} to="/detail/btc">
+                                    <span >Bitcoin</span> | BTC
+                                </Link>
+                            </td>
+                            <td className={styles.tdLabel}>
+                                R$14.1222
+                            </td>
+                            <td className={styles.tdLabel}>
+                                R$16.0705
+                            </td>
+                            <td className={styles.tdLoss}>
+                                <span>-5.3</span>
+                            </td>
+                        </tr>
+                    </tbody>
+
+                </table>
+
+        </main>
     )
 }
